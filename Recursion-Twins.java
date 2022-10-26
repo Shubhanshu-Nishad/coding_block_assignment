c457f47675679a9283e4988a695d72f1390d5ff0
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2022-10-26 18:41:17 +0530

    Create Recursion-Twins.java

---
+++ Recursion-Twins.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    static int count;
+
+	public static void main(String[] args) {
+		count = 0;
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.next();
+
+		count = countTwins(inp);
+		System.out.println(count);
+	}
+
+	public static int countTwins(String inp) {
+		if (inp.length() < 3)
+			return 0;
+
+		int count = 0;
+		if (inp.charAt(0) == inp.charAt(2) && inp.charAt(1) != inp.charAt(0))
+			count ++;
+
+		return count + countTwins(inp.substring(1));
+	}
+}
+//https://hack.codingblocks.com/app/contests/3553/258/problem
